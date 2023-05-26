a4ac700897ce90dfbc2fde96d2a03a40057654f8
AlejandroZala <galeza2012@hotmail.com>
2023-05-26 12:02:53 -0300

    cambio carts model

--- src/dao/mongo/models/carts.js
+++ src/dao/mongo/models/carts.js
@@ -2,20 +2,25 @@ import mongoose from "mongoose";
 
 const collection = "Carts";
 
-const schema = new mongoose.Schema(
-  {
+const schema = new mongoose.Schema({
     products: {
-      type:[
-        {
+      type:[{
           product:{
             type: mongoose.SchemaTypes.ObjectId,
             ref: "Products"
           },
-          quantity: Number
         }
       ],
-      default: []
-    }
+      default: [],
+    },
+    totalAmount:{
+      type:Number,
+      default: 0
+    },
+    user:{
+      type: String,
+      default: "UserCart"
+    } 
   },{ timestamps:{createdAt: "created_at", updatedAt: "updated_at"}}
 );
 
